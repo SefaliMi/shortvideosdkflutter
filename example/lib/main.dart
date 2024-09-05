e9c5f9916d7d2e6d78f2345b8ea2fdc7c70529aa
sefali <sefali.mistry@goswirl.live>
2024-09-05 15:45:52 +0530

    first commit

---
+++ example/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:swirl_shortvideosdk/swirl_shortvideosdk.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('plugin Example'),
+      ),
+      body: BoxImages(),
+    );
+  }
+}
---
+++ lib/swirl_shortvideosdk.dart
@@ -0,0 +1,17 @@
+library swirl_shortvideosdk;
+
+import 'package:flutter/material.dart';
+
+class BoxImages extends StatefulWidget {
+  const BoxImages({super.key});
+
+  @override
+  State<BoxImages> createState() => _BoxImagesState();
+}
+
+class _BoxImagesState extends State<BoxImages> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello");
+  }
+}
